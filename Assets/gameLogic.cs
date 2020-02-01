68bcdfffb91ed37429bf00926a2cc0bce93b183e
roccotg8 <rtg8@njit.edu>
2020-02-01 15:56:07 -0500

    trying to set player numbers to pass to other functions later on

--- Assets/gameLogic.cs
+++ Assets/gameLogic.cs
@@ -6,16 +6,39 @@ using Newtonsoft.Json.Linq;
 
 public class gameLogic : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start() {
         AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.onReady += OnReady;		
+		AirConsole.instance.onConnect += OnConnect;	
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void OnReady(string code){
+		//Since people might be coming to the game from the AirConsole store once the game is live, 
+		//I have to check for already connected devices here and cannot rely only on the OnConnect event 
+		List<int> connectedDevices = AirConsole.instance.GetControllerDeviceIds();
+		foreach (int deviceID in connectedDevices) {
+			AddNewPlayer (deviceID);
+		}
+	}
+
+    void OnConnect (int device){
+		AddNewPlayer (device);
+	}
+
+    private void AddNewPlayer(int deviceID){
+
+		/*if (players.ContainsKey (deviceID)) {
+			return;
+		}
+
+		//store device id + player script in a dictionary
+		players.Add(deviceID, newPlayer.GetComponent<Player_RoboRepair>());
+        */
+	}
+
     void OnMessage(int fromDeviceID, JToken data) {
         Debug.Log("message from" + fromDeviceID + ", data: " + data);
         if(data["action"] != null && data["action"].ToString().Equals("interact1")){
@@ -37,6 +60,8 @@ public class gameLogic : MonoBehaviour
         //unregister events
         if(AirConsole.instance != null){
             AirConsole.instance.onMessage -= OnMessage;
+            AirConsole.instance.onReady -= OnReady;		
+			AirConsole.instance.onConnect -= OnConnect;	
         }
     }
 }
