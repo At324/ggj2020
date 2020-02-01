cb48c7e0c1ecd9366d2e93d91733072359e82364
roccotg8 <rtg8@njit.edu>
2020-01-31 23:56:11 -0500

    starting to work on making a controller for our game

---
+++ Assets/gameLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class gameLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
