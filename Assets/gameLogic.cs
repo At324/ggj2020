c8aaba9ca54c9fc267dea060d66e5e15e6a35fd9
roccotg8 <rtg8@njit.edu>
2020-02-01 16:14:20 -0500

    input and player number are passed to another funciton. sets active players (max 8) at the start of the game

--- Assets/gameLogic.cs
+++ Assets/gameLogic.cs
@@ -7,12 +7,14 @@ using Newtonsoft.Json.Linq;
 public class gameLogic : MonoBehaviour
 {
 
-
+    public Dictionary<int, string[]> players = new Dictionary<int, string[]> (); 
+    string[] playerButtons = new string[4];
     // Start is called before the first frame update
     void Start() {
         AirConsole.instance.onMessage += OnMessage;
         AirConsole.instance.onReady += OnReady;		
 		AirConsole.instance.onConnect += OnConnect;	
+        AirConsole.instance.SetActivePlayers(8);
     }
 
     void OnReady(string code){
@@ -30,29 +32,42 @@ public class gameLogic : MonoBehaviour
 
     private void AddNewPlayer(int deviceID){
 
-		/*if (players.ContainsKey (deviceID)) {
+		if (players.ContainsKey (deviceID)) {
 			return;
 		}
-
+        
 		//store device id + player script in a dictionary
-		players.Add(deviceID, newPlayer.GetComponent<Player_RoboRepair>());
-        */
+		players.Add(deviceID, playerButtons);
+        
 	}
 
     void OnMessage(int fromDeviceID, JToken data) {
-        Debug.Log("message from" + fromDeviceID + ", data: " + data);
+        //Debug.Log("message from" + fromDeviceID + ", data: " + data);
+
+        //pass player number and data to other funtion
         if(data["action"] != null && data["action"].ToString().Equals("interact1")){
-            Camera.main.backgroundColor = new Color(255,0,0);
+            //Camera.main.backgroundColor = new Color(255,0,0);
+            //Debug.Log("hi");
+            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),"interact1");
         }
         if(data["action"] != null && data["action"].ToString().Equals("interact2")){
-            Camera.main.backgroundColor = new Color (0,255,0);
+            //Camera.main.backgroundColor = new Color (0,255,0);
+            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),"interact2");
         }
         if(data["action"] != null && data["action"].ToString().Equals("interact3")){
-            Camera.main.backgroundColor = new Color (0,0,255);
+            //Camera.main.backgroundColor = new Color (0,0,255);
+            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),"interact3");
         }
         if(data["action"] != null && data["action"].ToString().Equals("interact4")){
-            Camera.main.backgroundColor = new Color (255,255,0);
+            //Camera.main.backgroundColor = new Color (255,255,0);
+            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),"interact4");
         }
+        
+    }
+
+    void sendInput(int player, string button){
+        //do something based on player number and button they pressed
+        Debug.Log("player " + player + " pressed button " + button);
     }
 
 
@@ -62,6 +77,13 @@ public class gameLogic : MonoBehaviour
             AirConsole.instance.onMessage -= OnMessage;
             AirConsole.instance.onReady -= OnReady;		
 			AirConsole.instance.onConnect -= OnConnect;	
+        }
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            AirConsole.instance.SetActivePlayers(8);
+            Debug.Log("active players set");
         }
     }
 }
