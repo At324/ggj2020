c538104fea9c3cdc0ba6b6639cd4ae18a03f06a4
roccotg8 <rtg8@njit.edu>
2020-02-01 16:39:27 -0500

    cleaned up the code. removed some unused functions and wrote shorter if statements

--- Assets/gameLogic.cs
+++ Assets/gameLogic.cs
@@ -7,62 +7,21 @@ using Newtonsoft.Json.Linq;
 public class gameLogic : MonoBehaviour
 {
 
-    public Dictionary<int, string[]> players = new Dictionary<int, string[]> (); 
-    string[] playerButtons = new string[4];
     // Start is called before the first frame update
     void Start() {
         AirConsole.instance.onMessage += OnMessage;
-        AirConsole.instance.onReady += OnReady;		
-		AirConsole.instance.onConnect += OnConnect;	
+        //AirConsole.instance.onReady += OnReady;		
+		//AirConsole.instance.onConnect += OnConnect;	
         AirConsole.instance.SetActivePlayers(8);
     }
-
-    void OnReady(string code){
-		//Since people might be coming to the game from the AirConsole store once the game is live, 
-		//I have to check for already connected devices here and cannot rely only on the OnConnect event 
-		List<int> connectedDevices = AirConsole.instance.GetControllerDeviceIds();
-		foreach (int deviceID in connectedDevices) {
-			AddNewPlayer (deviceID);
-		}
-	}
-
-    void OnConnect (int device){
-		AddNewPlayer (device);
-	}
-
-    private void AddNewPlayer(int deviceID){
-
-		if (players.ContainsKey (deviceID)) {
-			return;
-		}
-        
-		//store device id + player script in a dictionary
-		players.Add(deviceID, playerButtons);
-        
-	}
 
     void OnMessage(int fromDeviceID, JToken data) {
         //Debug.Log("message from" + fromDeviceID + ", data: " + data);
 
-        //pass player number and data to other funtion
-        if(data["action"] != null && data["action"].ToString().Equals("interact1")){
-            //Camera.main.backgroundColor = new Color(255,0,0);
-            //Debug.Log("hi");
-            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),"interact1");
+        //pass player number and button pressed to other funtion
+        if(data["action"] != null){
+            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),data["action"].ToString());
         }
-        if(data["action"] != null && data["action"].ToString().Equals("interact2")){
-            //Camera.main.backgroundColor = new Color (0,255,0);
-            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),"interact2");
-        }
-        if(data["action"] != null && data["action"].ToString().Equals("interact3")){
-            //Camera.main.backgroundColor = new Color (0,0,255);
-            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),"interact3");
-        }
-        if(data["action"] != null && data["action"].ToString().Equals("interact4")){
-            //Camera.main.backgroundColor = new Color (255,255,0);
-            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),"interact4");
-        }
-        
     }
 
     void sendInput(int player, string button){
@@ -70,13 +29,10 @@ public class gameLogic : MonoBehaviour
         Debug.Log("player " + player + " pressed button " + button);
     }
 
-
     void OnDestroy (){
         //unregister events
         if(AirConsole.instance != null){
             AirConsole.instance.onMessage -= OnMessage;
-            AirConsole.instance.onReady -= OnReady;		
-			AirConsole.instance.onConnect -= OnConnect;	
         }
     }
 
