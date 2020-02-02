ce3c4cba1f30f5c0af2888b05cdb47e548904b20
tye <at324@njit.edu>
2020-02-01 22:57:38 -0500

    setting up airconsole with game assets, play/credits menu icons added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
 
 public class GameManager : MonoBehaviour
 {
@@ -54,13 +56,43 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.SetActivePlayers(8);
     }
 
-    // Update is called once per frame
+    void OnMessage(int fromDeviceID, JToken data)
+    {
+        //Debug.Log("message from" + fromDeviceID + ", data: " + data);
+
+        //pass player number and button pressed to other funtion
+        if (data["action"] != null)
+        {
+            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID), data["action"].ToString());
+        }
+    }
+
+    void sendInput(int player, string button)
+    {
+        //do something based on player number and button they pressed
+        Debug.Log("player " + player + " pressed button " + button);
+    }
+
+    void OnDestroy()
+    {
+        //unregister events
+        if (AirConsole.instance != null)
+        {
+            AirConsole.instance.onMessage -= OnMessage;
+        }
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AirConsole.instance.SetActivePlayers(8);
+            Debug.Log("active players set");
+        }
     }
 
     void FixedUpdate()
