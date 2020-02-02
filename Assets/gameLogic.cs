6ca49edc05ae8602e50308783ed94ff77bf88fd5
roccotg8 <rtg8@njit.edu>
2020-02-02 01:31:32 -0500

    considated into just one controller and one logic script. still need to enable swapping between controller layouts.

--- Assets/gameLogic.cs
+++ Assets/gameLogic.cs
@@ -16,8 +16,16 @@ public class gameLogic : MonoBehaviour
     void OnMessage(int fromDeviceID, JToken data) {
         //Debug.Log("message from" + fromDeviceID + ", data: " + data);
 
+        if(data["action"] != null && data["action"].ToString().Equals("play")){
+            //load game scene
+            Debug.Log("play game");
+        }
+        if(data["action"] != null && data["action"].ToString().Equals("credits")){
+            //load credits scene
+            Debug.Log("credits");
+        }
         //pass player number and button pressed to other funtion
-        if(data["action"] != null){
+        if(data["action"] != null && (data["action"].ToString().Equals("interact1") || data["action"].ToString().Equals("interact2") || data["action"].ToString().Equals("interact3") ||data["action"].ToString().Equals("interact4"))){
             sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),data["action"].ToString());
         }
     }
