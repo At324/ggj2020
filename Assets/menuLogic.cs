6d18220040cd2ebf8c868f54c5dbf575ccf72d81
roccotg8 <rtg8@njit.edu>
2020-02-01 20:43:19 -0500

    added menu logic and menu controller. next step is to make it only useable by player one

--- Assets/menuLogic.cs
+++ Assets/menuLogic.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 using NDream.AirConsole;
 using Newtonsoft.Json.Linq;
 
-public class gameLogic : MonoBehaviour
+public class menuLogic : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start() {
         AirConsole.instance.onMessage += OnMessage;
@@ -17,15 +16,16 @@ public class gameLogic : MonoBehaviour
         //Debug.Log("message from" + fromDeviceID + ", data: " + data);
 
         //pass player number and button pressed to other funtion
-        if(data["action"] != null){
-            sendInput(AirConsole.instance.ConvertDeviceIdToPlayerNumber(fromDeviceID),data["action"].ToString());
+        if(data["action"] != null && data["action"].ToString().Equals("play")){
+            //load game scene
+            Debug.Log("play game");
+        }
+        if(data["action"] != null && data["action"].ToString().Equals("credits")){
+            //load credits scene
+            Debug.Log("credits");
         }
     }
 
-    void sendInput(int player, string button){
-        //do something based on player number and button they pressed
-        Debug.Log("player " + player + " pressed button " + button);
-    }
 
     void OnDestroy (){
         //unregister events
